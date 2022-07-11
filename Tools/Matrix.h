b1e5b50174082806188f03979fd6ace758842b26
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-07-11 23:37:02 +0200

    Début Matrice

---
+++ Tools/Matrix.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef RAYGEN_MATRIX_H
+#define RAYGEN_MATRIX_H
+#include <array>
+#include "Point.h"
+#include "Vector.h"
+
+class Matrix {
+private:
+	std::array<float, 16> mat = {};
+public:
+	Matrix() {
+		mat = Matrix::identity();
+	};
+	Matrix(const Matrix& m) {
+		mat = m.mat;
+	};
+	~Matrix() = default;
+
+	std::array<float, 16> getMatrix() const;
+	static std::array<float, 16> identity();
+	Matrix inverse() const;
+	float operator()(const int& i, const int& j) const;
+	float& operator()(int i, int j);
+	Matrix operator*(const Matrix& mult) const;
+	Point operator*(const Point& p) const;
+	Vector operator*(const Vector& p) const;
+	Matrix operator*(const float& f) const;
+};
+std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+#endif //RAYGEN_MATRIX_H
