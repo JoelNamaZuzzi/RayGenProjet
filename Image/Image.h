a2950d3bedfdae3b0e62430a72de304d54522ef0
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-07-14 17:19:55 +0200

    Made Image.h, will do the .cpp with the reading, loading, etc... should not be that hard thanks to internet

--- Image/Image.h
+++ Image/Image.h
@@ -9,6 +9,8 @@
 #include"ImgType.h"
 #include "../Tools/Color.h"
 
+//instead of the usual, i will partially define the constructors in the .cpp to keep this file clear and define some function here
+
 class Image
 {
 private:
@@ -21,7 +23,75 @@ private:
 	std::array<int, 2> rigthtDown;
 	std::string path;
 public: 
+	Image(const std::string &filename);
+	Image(int w, int h, Color col) :width(w), height(h), channel(3) {
+		size = width * height * channel;
+		data = new uint8_t[size];
 
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				(*this)(x, y, col);
+			}
+		}
+	}
+	Image(const Image &img);
+	Image(int w, int h) : width(w), height(h), channel(3) {
+		size = width * height * channel;
+		data = new uint8_t[size];
+	}
+	~Image();
+
+	//Load image
+	bool read(const std::string &filename);
+	//Write image
+	bool write(const std::string &filename);
+
+	Color operator()(const int &x, const int &y) const;
+	Color operator()(const float &x, const float &y) const;
+	void operator()(const int &x, const int &y, const Color &color);
+
+	//image type
+	static getImgType(const std::string &filename);
+
+	int getWidth() const {
+		return width;
+	}
+
+	int getHeight() const {
+		return height;
+	}
+
+	inline int getChannel() const {
+		return channel;
+	}
+
+	inline size_t getSize() const {
+		return size;
+	}
+
+	inline uint8_t *getData() const {
+		return data;
+	}
+
+	inline std::string getPath() const {
+		return path;
+	}
+
+	inline std::array<int, 2> getLeftTop() {
+		return leftTop;
+	}
+
+	inline void setLeftTop(std::array<int, 2> rt) {
+		leftTop = rt;
+	}
+
+	inline std::array<int, 2> getRightBottom() {
+		return rightBottom;
+	}
+
+	inline void setRightBottom(std::array<int, 2> rb) {
+		rightBottom = rb;
+	}
 };
 
 #endif //RAYGEN_IMG_H
